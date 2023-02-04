f92d37a167333b11a2064bd3dd59d692cd4532ae
mktteee <122069270+mktteee@users.noreply.github.com>
2023-02-04 12:21:55 +0900

    1

---
+++ PHPjob/hello/index.php
@@ -0,0 +1,30 @@
+<?php
+$message = "Hello World!";
+echo $message;
+echo '<br>';
+
+define("ADMIN_EMAIL", "y-i-group@gmail.com");
+define("LIST_COUNT", 15);
+echo ADMIN_EMAIL;
+echo '<br>';
+echo LIST_COUNT;
+echo '<br>';
+
+echo 1 + 1;
+echo '<br>';
+echo 10 - 1;
+echo '<br>';
+echo 2*2;
+echo '<br>';
+echo 10 / 5;
+echo '<br>';
+echo 10 % 3;
+echo '<br>';
+
+$x = 0;
+$x++;
+echo $x;
+echo '<br>';
+$x *= 5;
+echo $x;
+?>
