ddad0b60d6b456a7873ae9d258f1af23eb5dc9f1
mktteee <122069270+mktteee@users.noreply.github.com>
2023-02-05 22:52:56 +0900

    1

--- PHPjob/hello/index.php
+++ PHPjob/hello/index.php
@@ -21,9 +21,77 @@ echo '<br>';
 echo 10 % 3;
 echo '<br>';
 
-$x = 1;
+$x = 0;
+$x *= 5;
+echo $x;
+echo '<br>';
+
 $x++;
 echo $x;
 echo '<br>';
+
+$x += 5;
+echo $x;
+echo '<br>';
+
+
+$age = 18;
+if ($age >= 20) {
+    echo "お酒が飲めるぞ！";
+} else {
+    echo "お酒は20歳になってから！";
+}
+echo '<br>';
+
+$name = "";
+echo ($name != "")?'名前を受け付けました':'名前を記入してください';
+echo '<br>';
+
+$name = 'taro';
+$password = 'pass';
+if ($name === 'taro' && $password === 'pass') {
+    echo 'ログイン成功です。';
+} elseif ($name === 'taro') {
+    echo 'パスワードが間違っています。';
+} elseif ($password === 'pass') {
+    echo '名前が間違っています。';
+} else {
+    echo '入力情報が間違っています。';
+}
+echo '<br>';
+
+$num = 0;
+while ($num < 10) {
+    echo $num;
+    $num++;
+    if($num == 5) {
+        break;
+    }
+}
+echo '<br>';
+
+$num = 0;
+do {
+    echo $num;
+    $num++;
+} while($num < 10);
+echo '<br>';
+
+for($i=0;$i<100;$i++) {
+    echo $i;
+    echo '<br>';
+}
+echo '<br>';
+
+$num = 0;
+while ($num < 100) {
+    echo $num;
+    echo '<br>';
+    $num++;
+    
+
+}
+
+
 
 ?>
