5c291c719fc4256c69c6a5d8adecf8c7eabaa29e
mktteee <122069270+mktteee@users.noreply.github.com>
2023-02-06 11:03:07 +0900

    FizzBuzz問題

--- PHPjob/3-1/FizzBuzz問題.php
+++ PHPjob/3-1/FizzBuzz問題.php
@@ -1,9 +1,19 @@
 <?php
-$num = 1;
-while($num < 100) {
-    echo $num;
-    echo '<br>';
-    if($num == $num *= 3){
+$n = 1;
+for ($n = 1; $n <= 100; $n++) {
+    
+    
+    if ($n %3 === 0 && $n %5 === 0) {
+        echo 'FizzBuzz!';
+        echo '<br>';
+    }elseif ($n %5 === 0) {
+        echo 'Buzz!';
+        echo '<br>';
+    }elseif ($n %3 === 0) {
         echo 'Fizz!';
+        echo '<br>';
+    }else{
+        echo $n;
+        echo '<br>';
     }
 }
