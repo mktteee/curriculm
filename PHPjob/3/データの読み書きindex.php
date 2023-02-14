512b62b961b24fc8e90b890908e33ac8b9d7b981
mktteee <122069270+mktteee@users.noreply.github.com>
2023-02-14 11:20:45 +0900

    データの読み書きチャレンジ

--- PHPjob/3/データの読み書きindex.php
+++ PHPjob/3/データの読み書きindex.php
@@ -1,6 +1,7 @@
 <?php
+    //書き込み
     $testFile = "test.txt";
-    $contents = "こんにちは！";
+    $contents = "おはよう！";
     
     if (is_writable ($testFile)) {
        
@@ -16,3 +17,25 @@
         echo "not writable!";
         exit;
     }
+
+    
+    //読み込み
+    $test_file = "test2.txt";
+    
+    if (is_readable($test_file)) {
+        
+        $fp = fopen($test_file, "r");
+        //開いたファイルを1行ずつ読み込む
+        while ($line = fgets($fp)) {
+           //改行コード込みで1行ずつ出力
+            echo $line.'<br>';
+        }
+        
+        fclose($fp);
+    } else {
+        
+        echo "not readable!";
+        exit;
+    }
+   
+?>
