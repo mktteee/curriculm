4de7bb1f7ae2f31ee6cbe1ea758491c315b050df
mktteee <122069270+mktteee@users.noreply.github.com>
2023-02-20 11:23:58 +0900

    1

--- PHPjob/3-4/answer.php
+++ PHPjob/3-4/answer.php
@@ -1,26 +1,25 @@
-<link rel= " stylesheet" href = "style.css">
-<body>
-    
-
+<link rel="stylesheet" href="style.css">
 <?php 
-
 //[question.php]から送られてきた名前の変数、選択した回答、問題の答えの変数を作成
 $my_name = $_POST['my_name'];
-
 $answer1 = $_POST['answer1'];
 $answer2 = $_POST['answer2'];
 $answer3 = $_POST['answer3'];
+$value1 = $_POST['value1'];
+$value2 = $_POST['value2'];
+$value3 = $_POST['value3'];
 //選択した回答と正解が一致していれば「正解！」、一致していなければ「残念・・・」と出力される処理を組んだ関数を作成する
-
+$result1= ($answer1 == $value1)?'正解！':'残念・・・';
+$result2= ($answer2 == $value2)?'正解！':'残念・・・';
+$result3= ($answer3 == $value3)?'正解！':'残念・・・';
 ?>
-<p><!--POST通信で送られてきた名前を表示-->さんの結果は・・・？</p>
+<p><?php echo $my_name; ?>さんの結果は・・・？</p>
 <p>①の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
-
+<?php echo $result1 ?>
 <p>②の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
-
+<?php echo $result2 ?>
 <p>③の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
-
-</body>
+<?php echo $result3 ?>
--- PHPjob/3-4/question.php
+++ PHPjob/3-4/question.php
@@ -1,48 +1,42 @@
-<div class ="all">
+<link rel="stylesheet" href="style.css">
 <?php
+//POST送信で送られてきた名前を受け取って変数を作成
 $my_name = $_POST['my_name'];
-
+//①画像を参考に問題文の選択肢の配列を作成してください。
 $select1 = [80,22,20,21];
 $select2 = ["PHP","Python","JAVA","HTML"];
 $select3 = ["join","select","insert","update"];
-
+//② ①で作成した、配列から正解の選択肢の変数を作成してください
 $answer1 = $select1[0];
 $answer2 = $select2[3];
 $answer3 = $select3[1];
 ?>
-<link rel="stylesheet" href="style.css">
-<body>
 <p>お疲れ様です<?php echo $my_name; ?>さん</p>
-
-<h2>①ネットワークのポート番号は何番？</h2>
+<!--フォームの作成 通信はPOST通信で-->
 <form action="answer.php" method="post">
+<h2>①ネットワークのポート番号は何番？</h2>
+<!--③ 問題のradioボタンを「foreach」を使って作成する-->
 <?php foreach ($select1 as $value1) { ?>
-    <input type="radio" name=/>
+    <input type="radio"/>
     <?php echo $value1;
     } ?>
-
 <h2>②Webページを作成するための言語は？</h2>
-
+<!--③ 問題のradioボタンを「foreach」を使って作成する-->
 <?php foreach ($select2 as $value2) { ?>
     <input type="radio"/>
     <?php echo $value2;
     } ?>
-
 <h2>③MySQLで情報を取得するためのコマンドは？</h2>
-
+<!--③ 問題のradioボタンを「foreach」を使って作成する-->
 <?php foreach ($select3 as $value3) { ?>
     <input type="radio"/>
     <?php echo $value3;
     } ?>
-
+<br>
+<input type="submit" value="回答"/>
 <!--問題の正解の変数と名前の変数を[answer.php]に送る-->
-
-    $my_name ='my_name'
-    $answer1 ='answer1'
-    $answer2 ='answer2'
-    $answer3 ='answer3'
-
-   
+<input type="hidden" name="my_name" value="'.$my_name.'"/>
+<input type="hidden" name='answer1' value="'.$answer1.'"/>
+<input type="hidden" name='answer2' value="'.$answer2.'"/>
+<input type="hidden" name='answer3' value="'.$answer3.'"/>
 </form>
-</body>
-</div>
